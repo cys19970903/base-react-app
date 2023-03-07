61e1ae16db0f10579074c763133e6f860447ec37
Spike Chen <cys19970903@gmail.com>
2023-03-07 18:29:01 +0800

    chore

--- config/webpack.config.base.ts
+++ config/webpack.config.base.ts
@@ -10,12 +10,23 @@ const config: Configuration = {
 		path: fileToPath('../src/index.tsx'),
 	},
 	output: {
-		filename: 'static/js/[name][chunkName].js',
+		filename: '../static/js/[name][chunkName].js',
 		path: fileToPath('../dist'),
 		clean: true,
 	},
 	module: {
-		rules: [{}],
+		rules: [
+			{
+				test: /\.(ts)x?$/,
+				exclude: /node_modules/,
+				options: {
+					presets: ['@babel/preset-react', '@babel/preset-typescript'],
+				},
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.tsx', '.ts'],
 	},
 };
 
