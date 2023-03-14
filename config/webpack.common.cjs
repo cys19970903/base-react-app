b39953d02d072c7645e3d19817265550c1defca5
Spike Chen <cys19970903@gmail.com>
2023-03-14 17:39:18 +0800

    chore：add react refresh

--- config/webpack.common.cjs
+++ config/webpack.common.cjs
@@ -16,80 +16,76 @@ module.exports = {
 				test: /\.(ts|tsx)$/,
 				exclude: /node_modules/,
 				use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-            },
-          },
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-            },
-          },
-        ],
+					{
+						loader: 'babel-loader',
+						options: {
+							cacheDirectory: true,
+						},
+					},
+					{
+						loader: 'ts-loader',
+						options: {
+							transpileOnly: true,
+						},
+					},
+				],
 			},
 			{
-        test: /\.less$/,
-        include: /src/,
-        exclude: /\.module\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader',
-        ],
-      },
-      {
-        test: /\.module\.less$/,
-        include: /src/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: { exportLocalsConvention: 'camelCase' },
-            },
-          },
-          'less-loader',
-        ],
-      },
-      {
-        test: /\.json$/,
+				test: /\.less$/,
+				include: /src/,
+				exclude: /\.module\.less$/,
+				use: ['style-loader', 'css-loader', 'less-loader'],
+			},
+			{
+				test: /\.module\.less$/,
+				include: /src/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							modules: { exportLocalsConvention: 'camelCase' },
+						},
+					},
+					'less-loader',
+				],
+			},
+			{
+				test: /\.json$/,
 				exclude: /node_modules/,
-        loader: 'json-loader',
-        type: 'json',
-      },
+				loader: 'json-loader',
+				type: 'json',
+			},
 			{
-        test: /\.(png|jpg|gif|svg|webp)$/i,
-        type: 'asset/resource',
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024,
-          },
-        },
-        generator: {
-          filename: 'images/[name]_[contenthash].[ext]',
-        },
-      },
+				test: /\.(png|jpg|gif|svg|webp)$/i,
+				type: 'asset/resource',
+				parser: {
+					dataUrlCondition: {
+						maxSize: 10 * 1024,
+					},
+				},
+				generator: {
+					filename: 'images/[name]_[contenthash].[ext]',
+				},
+			},
 			{
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name]_[contenthash].[ext]',
-        },
-      },
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'fonts/[name]_[contenthash].[ext]',
+				},
+			},
 		],
 	},
 	resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '../src'),
-    },
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+		alias: {
+			'@': path.resolve(__dirname, '../src'),
+		},
+		extensions: ['.tsx', '.ts', '.js', '.jsx'],
 	},
 	plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html'),
-    }),
-  ],
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, '../public/index.html'),
+		}),
+	],
 };
--- config/webpack.prod.cjs
+++ config/webpack.prod.cjs
@@ -6,103 +6,99 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.cjs');
 
 module.exports = require('webpack-merge').merge(common, {
-  mode: 'production',
-  devtool: false,
-  optimization: {
-    runtimeChunk: {
-      name: 'runtime',
-    },
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            drop_debugger: true,
-          },
-        },
-      }),
-      new CssMinimizerPlugin(),
-    ],
-    splitChunks: {
-      chunks: 'all',
-      maxSize: 500 * 1000,
-      cacheGroups: {
-        // use HtmlWebpackExternalsPlugin
-        // react: {
-        //   test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-        //   name: 'react',
-        //   chunks: 'all',
-        //   priority: 10,
-        // },
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          priority: 5,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.less$/,
-        include: /src/,
-        exclude: /\.module\.less$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'less-loader',
-        ],
-      },
-      {
-        test: /\.module\.less$/,
-        include: /src/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: { exportLocalsConvention: 'camelCase' },
-            },
-          },
-          'less-loader',
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style/[name].[contenthash].css',
-      chunkFilename: 'style/[id].[contenthash].css',
-    }),
-    new HtmlWebpackExternalsPlugin({
-      externals: [
-        {
-          module: 'react',
-          entry: 'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
-          global: 'React',
-        },
-        {
-          module: 'react-dom',
-          entry: 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
-          global: 'ReactDOM',
-        },
-        {
-          module: 'react-router-dom',
-          entry: 'https://cdnjs.cloudflare.com/ajax/libs/react-router-dom/6.9.0/react-router-dom.production.min.js',
-          global: 'ReactRouterDOM',
-        },
-      ],
-    }),
-  ],
+	mode: 'production',
+	devtool: false,
+	optimization: {
+		runtimeChunk: {
+			name: 'runtime',
+		},
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				extractComments: false,
+				terserOptions: {
+					compress: {
+						drop_console: true,
+						drop_debugger: true,
+					},
+				},
+			}),
+			new CssMinimizerPlugin(),
+		],
+		splitChunks: {
+			chunks: 'all',
+			maxSize: 500 * 1000,
+			cacheGroups: {
+				// use HtmlWebpackExternalsPlugin
+				// react: {
+				//   test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+				//   name: 'react',
+				//   chunks: 'all',
+				//   priority: 10,
+				// },
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendor',
+					priority: 5,
+				},
+				default: {
+					minChunks: 2,
+					priority: -20,
+					reuseExistingChunk: true,
+				},
+			},
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.less$/,
+				include: /src/,
+				exclude: /\.module\.less$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+			},
+			{
+				test: /\.module\.less$/,
+				include: /src/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							modules: { exportLocalsConvention: 'camelCase' },
+						},
+					},
+					'less-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new BundleAnalyzerPlugin({
+			openAnalyzer: false,
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'style/[name].[contenthash].css',
+			chunkFilename: 'style/[id].[contenthash].css',
+		}),
+		new HtmlWebpackExternalsPlugin({
+			externals: [
+				{
+					module: 'react',
+					entry: 'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
+					global: 'React',
+				},
+				{
+					module: 'react-dom',
+					entry: 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
+					global: 'ReactDOM',
+				},
+				{
+					module: 'react-router-dom',
+					entry: 'https://cdnjs.cloudflare.com/ajax/libs/react-router-dom/6.9.0/react-router-dom.production.min.js',
+					global: 'ReactRouterDOM',
+				},
+			],
+		}),
+	],
 });
